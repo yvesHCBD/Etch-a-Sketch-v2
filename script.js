8ce98d376260dc0af0bcedbcaed428ae07b87806
yves <64351866+yveslapuz@users.noreply.github.com>
2023-06-22 18:24:29 -0700

    Created very basic etch-a-sketch function of adding black to window

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ body.style.flexDirection = "column";
 body.style.alignItems = "center";
 body.style.justifyContent = "space-evenly";
 body.style.margin = "0";
-body.style.border = "2px solid green";
 
 const bodyContainer = document.createElement("div");
 bodyContainer.classList.add("bodyContainer");
@@ -16,7 +15,7 @@ body.appendChild(bodyContainer);
 const h1 = document.createElement("h1");
 h1.classList.add("title");
 h1.textContent = "Etch-a-Sketch";
-h1.style.margin = "0";
+h1.style.margin = "10px";
 
 const etchContainer = document.createElement("div");
 etchContainer.classList.add("etchContainer");
@@ -29,22 +28,32 @@ etchContainer.style.display = "flex";
 bodyContainer.appendChild(h1);
 bodyContainer.appendChild(etchContainer);
 
-for (let i = 0; i < 16; i++) {
-  const column = document.createElement("div");
-  column.classList.add("column");
-  for (let j = 0; j < 16; j++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    etchContainer.appendChild(cell);
-    cell.style.margin = "0";
-    cell.style.outline = "0.01px solid black";
-    cell.style.display = "flex";
-    cell.style.flex = "1 1 0";
-    cell.style.height = "25px";
-    cell.style.width = "25px";
-    cell.style.backgroundColor = "red";
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 0; j < num; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      etchContainer.appendChild(cell);
+      cell.style.margin = "0";
+      cell.style.outline = "0.01px solid black";
+      cell.style.display = "flex";
+      cell.style.flex = "1 1 0";
+      cell.style.height = "25px";
+      cell.style.width = "25px";
 
-    column.appendChild(cell);
+      column.appendChild(cell);
+    }
+    etchContainer.appendChild(column);
   }
-  etchContainer.appendChild(column);
 }
+createGrid(16);
+
+const gridDivs = document.querySelectorAll(".cell");
+
+gridDivs.forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "black";
+  });
+});
