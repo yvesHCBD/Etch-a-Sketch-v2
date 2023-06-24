51026a2eeb2a798227a71623c98f4098be0c8640
yves <64351866+yveslapuz@users.noreply.github.com>
2023-06-23 20:19:31 -0700

    Added eraser, clear whole window, and paint buttons with added functionality for each

--- script.js
+++ script.js
@@ -35,24 +35,21 @@ function createGrid(num) {
   for (let i = 0; i < num; i++) {
     const column = document.createElement("div");
     column.classList.add("column");
-    column.style.flex = "1";
     for (let j = 0; j < num; j++) {
       let cell = document.createElement("div");
       cell.classList.add("cell");
-      // etchContainer.appendChild(cell);
+      etchContainer.appendChild(cell);
       cell.style.margin = "0";
       cell.style.outline = "0.01px solid black";
       cell.style.display = "flex";
       // cell.style.flex = "0 1 0";
-      cell.style.height = `${400 / num}px`;
+      cell.style.height = `${400 / num}px`; //always makes the dimension of each cell fit equally within the drawing window
       cell.style.width = `${400 / num}px`;
       column.appendChild(cell);
     }
     etchContainer.appendChild(column);
   }
 }
-
-createGrid(16); //default size
 
 gridSizebtn.addEventListener("click", () => {
   document.querySelectorAll(".cell").forEach((e) => {
@@ -62,27 +59,15 @@ gridSizebtn.addEventListener("click", () => {
   let size = prompt(`Enter size of grid: 
   (ex: 16 means 16x16 grid)`);
   createGrid(size);
-
-  const gridDivs = document.querySelectorAll(".cell");
-
-  gridDivs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "black";
-    });
-  }); //allows for etch-a-sketch like functionality onto the window
+  paint();
 });
 
 bodyContainer.appendChild(h1);
 bodyContainer.appendChild(etchContainer);
 bodyContainer.appendChild(gridSizebtn);
 
-const gridDivs = document.querySelectorAll(".cell");
-
-gridDivs.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "black";
-  });
-}); //allows for etch-a-sketch like functionality onto the window
+createGrid(16); //default size
+paint();
 
 const resetGrid = document.createElement("button");
 resetGrid.classList.add("resetGrid-btn");
@@ -97,11 +82,57 @@ resetGrid.addEventListener("click", () => {
     e.remove();
   });
   createGrid(16);
-  const gridDivs = document.querySelectorAll(".cell");
+  paint();
+});
 
+function removePaint() {
+  const gridDivs = document.querySelectorAll(".cell");
+  gridDivs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "";
+    });
+  });
+}
+
+function paint() {
+  const gridDivs = document.querySelectorAll(".cell");
   gridDivs.forEach((div) => {
     div.addEventListener("mouseover", () => {
       div.style.backgroundColor = "black";
     });
   });
-});
+}
+
+function clear() {
+  const gridDivs = document.querySelectorAll(".cell");
+  gridDivs.forEach((div) => {
+    div.style.backgroundColor = "";
+  });
+}
+
+const eraser = document.createElement("button");
+eraser.classList.add("eraser-btn");
+eraser.textContent = "Eraser";
+eraser.style.marginLeft = "5px";
+
+eraser.addEventListener("click", removePaint);
+
+bodyContainer.appendChild(eraser);
+
+const clearWindow = document.createElement("button");
+clearWindow.classList.add("clearWindow-btn");
+clearWindow.textContent = "Clear Window";
+clearWindow.style.marginLeft = "5px";
+
+clearWindow.addEventListener("click", clear);
+
+bodyContainer.appendChild(clearWindow);
+
+const paintbtn = document.createElement("button");
+paintbtn.classList.add("paint-btn");
+paintbtn.textContent = "Paint";
+paintbtn.style.marginLeft = "5px";
+
+paintbtn.addEventListener("click", paint);
+
+bodyContainer.appendChild(paintbtn);
